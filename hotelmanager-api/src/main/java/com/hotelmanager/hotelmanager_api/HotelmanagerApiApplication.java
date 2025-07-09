5919a7b93bc248794a6eb3247c4bd346379eab4e
HamzaOuba00-1 <hamza.ouba.2004@gmail.com>
2025-07-09 20:41:35 +0200

    Initial commit: setup fullstack structure

---
+++ hotelmanager-api/src/main/java/com/hotelmanager/hotelmanager_api/HotelmanagerApiApplication.java
@@ -0,0 +1,13 @@
+package com.hotelmanager.hotelmanager_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelmanagerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelmanagerApiApplication.class, args);
+	}
+
+}
